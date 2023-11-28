73f1eb538cd69a6c7785c71b5facbc649673f929
Fujinuma <c0a22125e5@edu.teu.ac.jp>
2023-11-28 14:16:58 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import sys
+import random
 import pygame as pg
 
 
@@ -6,11 +7,17 @@ WIDTH, HEIGHT = 1600, 900
 
 
 def main():
-    pg.display.set_caption("逃げろ！こうかとん")#ウィンドウタイトルを「逃げろ！こうかとん」とする
-    screen = pg.display.set_mode((WIDTH, HEIGHT))#幅1600✖高さ900のスクリーンSurfaceを生成する
-    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")#背景画像pg_bg.jpgのSurfaceを生成する
-    kk_img = pg.image.load("ex02/fig/3.png")#こうかとん画像3.pngをロード
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)# 2倍に拡大したSurfaceを生成する
+    pg.display.set_caption("逃げろ！こうかとん")  # ウィンドウタイトルを「逃げろ！こうかとん」
+    screen = pg.display.set_mode((WIDTH, HEIGHT))  # 幅1600✖高さ900のスクリーンSurfaceを生成
+    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")  # 背景画像pg_bg.jpgのSurfaceを生成
+    kk_img = pg.image.load("ex02/fig/3.png")  # こうかとん画像3.pngをロード
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)  # 2倍に拡大したSurfaceを生成
+    bb_img = pg.Surface((20, 20))  # 練習:1 透明のsurfaceを生成
+    bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤い円を生成
+    bb_rct = bb_img.get_rect()  # 練習2:爆弾surfaceのRectを抽出
+    bb_rct.centerx = random.randint(0, WIDTH)  # 座標の設定
+    bb_rct.centery = random.randint(0, HEIGHT)
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -18,11 +25,12 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
-        screen.blit(bg_img, [0, 0])#背景画像をスクリーンSurfaceに貼り付ける
-        screen.blit(kk_img, [900, 400])#こうかとん画像をスクリーンSurfaceの横900，縦400に貼り付ける
-        pg.display.update()#画面を更新する
+        screen.blit(bg_img, [0, 0])  # 背景画像をスクリーンSurfaceに貼り付ける
+        screen.blit(kk_img, [900, 400])  # こうかとん画像をスクリーンSurfaceの横900，縦400に貼り付ける
+        screen.blit(bb_img, bb_rct)
+        pg.display.update()  # 画面を更新する
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
